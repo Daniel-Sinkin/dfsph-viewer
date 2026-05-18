b94a25185110a9d3c546d170f40c7198ad1fc4bd
Daniel-Sinkin <danielsinkin97@gmail.com>
2026-05-18 11:55:29 +0200

    Updated to new template

--- src/development_markers.hpp
+++ src/development_markers.hpp
@@ -1,6 +1,41 @@
+// src/development_markers.hpp
+
+#ifndef DFSPH_VIEWER_SRC_DEVELOPMENT_MARKERS_HPP
+#define DFSPH_VIEWER_SRC_DEVELOPMENT_MARKERS_HPP
+
 #pragma once
+
+#include <type_traits>
+
+#if defined(mut)
+#    error "mut marker macro is already defined"
+#endif
+
+#if defined(cpy)
+#    error "cpy marker macro is already defined"
+#endif
+
+#if defined(def)
+#    error "def marker macro is already defined"
+#endif
+
+#if defined(__clang__)
+#    define mut [[clang::annotate("mut")]]
+#    define cpy [[clang::annotate("cpy")]]
+#else
+#    define mut
+#    define cpy
+#endif
+
+#define def auto
 
 namespace dfsph_viewer
 {
-inline constexpr auto project_name = "dfsph-viewer";
+template <typename T>
+[[nodiscard]] constexpr def copy(const T& value) -> std::remove_cvref_t<T>
+{
+    return value;
+}
 }  // namespace dfsph_viewer
+
+#endif  // DFSPH_VIEWER_SRC_DEVELOPMENT_MARKERS_HPP
